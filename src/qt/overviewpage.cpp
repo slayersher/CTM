1a6fe6b08724b1ef5e3e533e5074c1631dac5c5c
slayersher <slayersher@gmail.com>
2014-05-06 11:19:46 -0700

    Update stats url.

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -234,6 +234,6 @@ void OverviewPage::finished(QNetworkReply *reply) {
 }
 
 void OverviewPage::DoHttpGet() {
-  QString url = "http://stats.continuum.com/news1.php";
+  QString url = "http://stats.continuumcoin.com/news1.php";
   nam->get(QNetworkRequest(QUrl(url)));
 }
